7cc09cd3d78450fb06789978b7c50543eacd7887
faeze1379 <fazyfirozifar@gmail.com>
2026-08-18 14:08:18 +0330

    feature(ui): add not found and loading component

---
+++ app/components/common/Loading.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="py-8 text-left text-slate-600">Is Loading...</div>
+</template>
---
+++ app/components/common/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="p-8 text-center text-slate-600">
+    <h2 class="text-lg font-bold text-slate-950">
+      {{ title }}
+    </h2>
+    <div v-if="description" class="mt-2 text-sm text-slate-500">
+      {{ description }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface IProps {
+  title: string;
+  description?: string;
+}
+
+withDefaults(defineProps<IProps>(), {
+  title: "",
+  description: "",
+});
+</script>
